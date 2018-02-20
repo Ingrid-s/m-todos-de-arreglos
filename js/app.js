31313f87366d3c7948c3f2dafa87442c4530d774
ingridsag <starlling@hotmail.com>
2018-02-20 10:44:01 -0600

    avances

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//1 ) Crea tu propia función forEach que funcione igual a Array.forEach. Algo asi:
+ var miForEach = [1, 2, 3, 4];
+for (var i=0; i<miForEach.length; i++){
+ console.log(miForEach [i]);
+   }
+
+  // Crea tu propia función map que funcione igual a Array.map. Algo asi:
+  var miForEach = [1, 2, 3, 4];
+for (var i=0; i<miForEach.length; i++){
+ console.log(miForEach [i]);
+   }
+   var newArray = [];
+  var miMap = newArray.push(miForEach);
+  console.log(miMap);
